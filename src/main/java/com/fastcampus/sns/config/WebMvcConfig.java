0a435e1c8e9dea4bcfcd8c92a363d15b5f30512d
SeongHoonLim <hooney200@gmail.com>
2025-06-30 23:30:07 +0900

    feat : Add copying of frontend static resources and build integration to Gradle builds

---
+++ src/main/java/com/fastcampus/sns/config/WebMvcConfig.java
@@ -0,0 +1,21 @@
+package com.fastcampus.sns.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        // "/feed", "/modify-post", "/post-detail" 같은 경로는 모두 index.html 로 포워딩
+        registry.addViewController("/{spring:\\w+}")
+                .setViewName("forward:/index.html");
+        registry.addViewController("/**/{spring:\\w+}")
+                .setViewName("forward:/index.html");
+        registry.addViewController("/{spring:\\w+}/**{spring:?!(\\.js|\\.css|\\.png|\\.jpg|\\.svg)$}")
+                .setViewName("forward:/index.html");
+    }
+
+}
